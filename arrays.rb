2d23419653e5706d1a4e00802271a13ba322dee4
Spence Peacock <spencerpeacock@gmail.com>
2017-06-05 16:58:57 -0500

    ruby excercises

---
+++ arrays.rb
@@ -0,0 +1,13 @@
+#a = ["john", "jacob", "jingle", "heimber", "Schmidt"]
+#puts a[0]
+#a.at(2)
+#a.fetch(3)
+#a.delete(4)
+#puts a.length
+#puts a.reverse
+#puts a.sort
+#puts a.slice(3)
+#puts a.shuffle
+#puts a.join("yeah!")
+#puts a.insert("his name is imy name too")
+#puts a.insert(0, "his name is my name too")
